323b6b47f468646452db41be874f64780b784c17
Stas <sidux77@gmail.com>
2022-03-09 18:51:24 +0100

    added repairments

--- Zestaw2/client/helloClient.c
+++ Zestaw2/client/helloClient.c
@@ -10,9 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <signal.h>
 #include <string.h>
-#include <stdbool.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
