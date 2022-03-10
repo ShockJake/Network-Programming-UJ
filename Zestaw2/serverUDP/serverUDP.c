e375c07ba095039a01fc73476f6bf5cd0a548b1f
Stas <sidux77@gmail.com>
2022-03-10 12:57:23 +0100

    Additions

--- Zestaw2/serverUDP/serverUDP.c
+++ Zestaw2/serverUDP/serverUDP.c
@@ -1,9 +1,9 @@
 /*
-    Spróbuj napisać parę klient-serwer komunikującą się przy pomocy protokołu UDP. 
-    Pamiętaj o tym, że UDP nie nawiązuje połączeń, 
-    więc to klient będzie musiał jako pierwszy wysłać jakiś datagram, a serwer na niego odpowie. 
-    
-    Sprawdź, czy możliwe jest wysyłanie pustych datagramów (tzn. o długości zero bajtów) — 
+    Spróbuj napisać parę klient-serwer komunikującą się przy pomocy protokołu UDP.
+    Pamiętaj o tym, że UDP nie nawiązuje połączeń,
+    więc to klient będzie musiał jako pierwszy wysłać jakiś datagram, a serwer na niego odpowie.
+
+    Sprawdź, czy możliwe jest wysyłanie pustych datagramów (tzn. o długości zero bajtów) —
     jeśli tak, to może niech klient właśnie taki wysyła?
 */
 
@@ -17,10 +17,12 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+#define MAXLINE 1024
+
 int createSocket()
 {
     int result = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if(result == -1)
+    if (result == -1)
     {
         perror("Can't create a socket");
         _exit(-1);
@@ -28,20 +30,20 @@ int createSocket()
     return result;
 }
 
-bool verifyConnection(int sockD)
+bool verifyConnection(int sockD, char buf[], int lenght, struct sockaddr* sockaddr_in)
 {
-    const char* data = "o";
-    while (true)
-    {
-        sendto(sockD, );
-    }
-    
+    int n;
+    n = recvfrom(sockD, buf, MAXLINE, MSG_WAITALL, &sockaddr_in, &lenght);
+
 }
 
 void startUDPServer(int port)
 {
     // Tworzenie deskryptora dla soketu
     int sd = createSocket();
+
+    // Główny buffor
+    char buf[MAXLINE];
 
     struct sockaddr_in server_addr, client_addr;
 
@@ -57,16 +59,21 @@ void startUDPServer(int port)
     server_addr.sin_port = htons(port);
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    
+    if (bind(sd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
+        perror("Can't bind the socket");
+        _exit(-1);
+    }
 
     int n, len;
 
+    len = sizeof(client_addr);
 
 
 }
 
 int main(int argc, char const *argv[])
 {
-    
+
     return 0;
 }
