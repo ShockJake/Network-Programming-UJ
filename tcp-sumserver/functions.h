44adcea40f57a87149221d490dfd3ce5bc285bf1
Stas <sidux77@gmail.com>
2022-04-12 17:45:21 +0200

    Moved functions

--- tcp-sumserver/functions.h
+++ tcp-sumserver/functions.h
@@ -6,9 +6,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/signal.h>
+#include <limits.h>
+#include <stdint.h>
+#include <errno.h>
 
 bool isNumber(char input);
 void sendError(int asd);
 void addEnding(char *data);
+int createSocket(int port);
+unsigned long long int sumNumbers(char *data);
+void sendData(unsigned long long int answer_int, int clientDescriptor);
+bool performAction(unsigned long long int *number, int cd);
 
 #endif // FUNCTIONS_H
--- tcp-sumserver/sumServer.c
+++ tcp-sumserver/sumServer.c
@@ -1,27 +1,7 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/signal.h>
-#include <limits.h>
-#include <stdint.h>
-#include <errno.h>
 #include "functions.h"
 
 // Socket descriptor
 int sd;
-
-int createSocket()
-{
-    // Socket creation with UDP specifications
-    int result = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (result == -1)
-    {
-        perror("Can't create socket");
-        exit(1);
-    }
-    return result;
-}
 
 // Function to close socket
 void sig_handler(int signum)
@@ -36,119 +16,15 @@ void sig_handler(int signum)
     exit(0);
 }
 
-unsigned long long int sumNumbers(char *data)
-{
-    unsigned long long int number = 0;
-    unsigned long long int result = 0;
-
-    char *numberStr = strtok(data, " ");
-    while (numberStr != NULL)
-    {
-
-        number = strtoull(numberStr, NULL, 10);
-        if (number == ULLONG_MAX && errno == ERANGE)
-        {
-            return -1;
-        }
-        if (number > INT_MAX - result)
-        {
-            return -1;
-        }
-        result += number;
-        numberStr = strtok(NULL, " ");
-    }
-    return result;
-}
-
-void sendData(unsigned long long int answer_int, int clientDescriptor)
-{
-    ssize_t byteN;
-    char answer_ch[12];
-    byteN = sprintf(answer_ch, "%lld", answer_int);
-    if (byteN == -1)
-    {
-        perror("Can't convert data");
-        sendError(clientDescriptor);
-        return;
-    }
-    addEnding(answer_ch);
-    printf("Result: %s", answer_ch);
-
-    // Sending answer
-    byteN = write(clientDescriptor, answer_ch, sizeof(answer_ch));
-    if (byteN == -1)
-    {
-        perror("Can't send data");
-    }
-    printf("Answer was sent successfully\n============================\n");
-}
-
-bool performAction(unsigned long long int *number, int cd)
-{
-    signed short int space_counter = 0;
-    char input[2] = {0, 0};
-    char message[1024];
-    ssize_t byteN;
-
-    memset(message, 0, sizeof(message));
-
-    while (byteN = read(cd, input, sizeof(input) - 1) != 0)
-    {
-        if (byteN == -1)
-        {
-            perror("Can't read data");
-            return false;
-        }
-        if (isNumber(input[0]))
-        {
-            space_counter = 0;
-            strncat(message, input, 1);
-        }
-        if (input[0] == ' ')
-        {
-            space_counter++;
-            if (space_counter > 1)
-            {
-                return false;
-            }
-            strcat(message, input);
-        }
-        if (input[0] == '\r')
-        {
-            continue;
-        }
-        if (input[0] == '\n')
-        {
-            printf("Message form the server: %s\n", message);
-            space_counter = 0;
-            *number = sumNumbers(message);
-
-            memset(input, 0, sizeof(input));
-            memset(message, 0, sizeof(message));
-
-            if (*number == -1 || *number == 0)
-            {
-                return false;
-            }
-            sendData(*number, cd);
-        }
-        memset(input, 0, sizeof(input));
-    }
-    return true;
-}
-
 void startServer(int port)
 {
     // Creating socket
-    sd = createSocket();
+    sd = createSocket(port);
 
     bool isActive = true;
 
     // Ilość opracowanych bajtów
     ssize_t byteN;
-
-    // Structure dor server data
-    struct sockaddr_in addres;
 
     // Accepted socket descriptor
     int clientDescriptor;
@@ -161,29 +37,6 @@ void startServer(int port)
 
     // Number for sending in char* type
     char answer_ch[12];
-
-    // Filling sockaddr_in
-    memset(&addres, 0, sizeof(addres));
-    addres.sin_family = AF_INET;
-    addres.sin_port = htons(port);
-    addres.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    // Cast to const struct sockaddr for passing into functions
-    const struct sockaddr *addr = (const struct sockaddr *)&addres;
-
-    // Bining socket
-    if (bind(sd, addr, sizeof(addres)) == -1)
-    {
-        perror("Can't bind socket");
-        exit(1);
-    }
-
-    // Making socket to listen
-    if (listen(sd, 10) == -1)
-    {
-        perror("Can't make the socket to listen");
-        exit(1);
-    }
 
     printf("\nServer is created, and listening on this port: %d\n", port);
 
